0bd6c543c2fb166a66de189a1a93bf7886adc7aa
CHONK12 <115587871+CHONK12@users.noreply.github.com>
2022-10-11 14:02:54 -0600

    Add files via upload

---
+++ Blue.ps1
@@ -0,0 +1,77 @@
+$MyWallpaper="https://github.com/CHONK12/rickRoll/blob/main/Bluelobster.jpg"
+$code = @'
+using System.Runtime.InteropServices;
+namespace Win32{
+
+     public class Wallpaper{
+        [DllImport("user32.dll", CharSet=CharSet.Auto)]
+         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ;
+
+         public static void SetWallpaper(string thePath){
+            SystemParametersInfo(20,0,thePath,3);
+         }
+    }
+ }
+'@
+
+add-type $code
+[Win32.Wallpaper]::SetWallpaper($MyWallpaper)
+
+Add-Type -TypeDefinition @'
+using System.Runtime.InteropServices;
+[Guid("5CDF2C82-841E-4546-9722-0CF74078229A"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IAudioEndpointVolume
+{
+    // f(), g(), ... are unused COM method slots. Define these if you care
+    int f(); int g(); int h(); int i();
+    int SetMasterVolumeLevelScalar(float fLevel, System.Guid pguidEventContext);
+    int j();
+    int GetMasterVolumeLevelScalar(out float pfLevel);
+    int k(); int l(); int m(); int n();
+    int SetMute([MarshalAs(UnmanagedType.Bool)] bool bMute, System.Guid pguidEventContext);
+    int GetMute(out bool pbMute);
+}
+[Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IMMDevice
+{
+    int Activate(ref System.Guid id, int clsCtx, int activationParams, out IAudioEndpointVolume aev);
+}
+[Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IMMDeviceEnumerator
+{
+    int f(); // Unused
+    int GetDefaultAudioEndpoint(int dataFlow, int role, out IMMDevice endpoint);
+}
+[ComImport, Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")] class MMDeviceEnumeratorComObject { }
+public class Audio
+{
+    static IAudioEndpointVolume Vol()
+    {
+        var enumerator = new MMDeviceEnumeratorComObject() as IMMDeviceEnumerator;
+        IMMDevice dev = null;
+        Marshal.ThrowExceptionForHR(enumerator.GetDefaultAudioEndpoint(/*eRender*/ 0, /*eMultimedia*/ 1, out dev));
+        IAudioEndpointVolume epv = null;
+        var epvid = typeof(IAudioEndpointVolume).GUID;
+        Marshal.ThrowExceptionForHR(dev.Activate(ref epvid, /*CLSCTX_ALL*/ 23, 0, out epv));
+        return epv;
+    }
+    public static float Volume
+    {
+        get { float v = -1; Marshal.ThrowExceptionForHR(Vol().GetMasterVolumeLevelScalar(out v)); return v; }
+        set { Marshal.ThrowExceptionForHR(Vol().SetMasterVolumeLevelScalar(value, System.Guid.Empty)); }
+    }
+    public static bool Mute
+    {
+        get { bool mute; Marshal.ThrowExceptionForHR(Vol().GetMute(out mute)); return mute; }
+        set { Marshal.ThrowExceptionForHR(Vol().SetMute(value, System.Guid.Empty)); }
+    }
+}
+'@
+
+[audio]::Volume  = 1.0 
+
+start microsoft-edge:https://www.yout-ube.com/watch?v=P9Sfyw-KhnM
+$wshell = New-Object -ComObject wscript.shell;
+$wshell.AppActivate('Google - Microsoft Edge')
+Sleep 2
+$wshell.SendKeys('{F11}')
